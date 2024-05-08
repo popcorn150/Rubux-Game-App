2453f6c1e9ca4e91a705953a2afa14fcf0000192
popcorn150 <hungrymanchannel@gmail.com>
2024-05-08 16:39:43 +0100

    moved the ship - added the aliens - moved the aliens

--- src/app.js
+++ src/app.js
@@ -22,6 +22,21 @@ let ship = {
 }
 
 let shipImg;
+let shipVelocityx = tileSize; //ship moving speed
+
+/**Aliens */
+let alienArray = [];
+let alienWIdth = tileSize * 2;
+let alienHeight = tileSize;
+let alienX = tileSize;
+let alienY = tileSize;
+let alienImg;
+
+let alienRows = 2;
+let alienColoumns = 3;
+let alienCount = 0; //number of aliens to defeat
+
+let alienVelocityX = 1; //alien moving speed
 
 window.onload = function () {
     board = document.getElementById("playingBoard");
@@ -37,4 +52,67 @@ window.onload = function () {
     shipImg.onload = function () {
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     };
+
+    alienImg = new Image();
+    alienImg.src = "/imgs/special-invader.png";
+    createAliens();
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveShip);
+}
+
+function update() {
+    requestAnimationFrame(update);
+
+    //clearing the canvas after each ship movement
+    context.clearRect(0, 0, board.width, board.height);
+
+    //redrawing the ship over again on the canvas
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+    //aliens
+    for (let i = 0; i < alienArray.length; i++) {
+        let alien = alienArray[i];
+        if (alien.alive) {
+            alien.x += alienVelocityX;
+
+            //if alien touches the borders
+            if (alien.x + alien.width >= board.width || alien.x <= 0){
+                alienVelocityX *= -1;
+                alien.x += alienVelocityX * 2;
+
+                //move all aliens down by one row
+                for (j = 0; j < alienArray.length; j++){
+                    alienArray[j].y += alienHeight;
+                }
+            }
+            context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+        }
+    }
+}
+
+function moveShip(e) {
+    if (e.code == "ArrowLeft" && ship.x - shipVelocityx >= 0) {//check if ship is moving out of bounds on the left
+        ship.x -= shipVelocityx; //moving the ship left one tile
+    }
+    else if (e.code == "ArrowRight" && ship.x + shipVelocityx + ship.width <= board.width) { //check if ship is moving out of bounds on the right
+        ship.x += shipVelocityx; //moving the ship right one tile
+    }
+}
+
+function createAliens() {
+    for (let c = 0; c < alienColoumns; c++) {
+        for (let r = 0; r < alienRows; r++) {
+            let alien = {
+                img: alienImg,
+                x: alienX + c * alienWIdth,
+                y: alienY + r * alienHeight,
+                width: alienWIdth,
+                height: alienHeight,
+                alive: true
+            }
+            alienArray.push(alien);
+        }
+    }
+    alienCount = alienArray.length;
 }
